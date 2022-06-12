603daf4ea76b4b98bccd00ef4f2610fed7af21e7
Vairamuthu M <mvairamuthu20000@gmail.com>
2022-06-12 20:44:34 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const connectDB = require("./config/db");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -11,12 +12,14 @@ const app = express();
 //Connect to database
 connectDB();
 
+app.use(cors());
+
 app.use(
-    "/graphql",
-    graphqlHTTP({
-        schema,
-        graphiql: process.env.NODE_ENV === "development",
-    })
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
 );
 
 app.listen(port, console.log(`Server running on port ${port}`));
